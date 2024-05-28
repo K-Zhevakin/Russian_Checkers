b0ced097f287fd7dae52d000a19e28708ed63775
Жевакин Кирилл <k.zhevakin@uniyar.ac.ru>
2024-05-28 18:04:35 +0300

    Добавлена отрисовка дисплея

--- Russian_Checkers/Russian_Checkers.cpp
+++ Russian_Checkers/Russian_Checkers.cpp
@@ -2,10 +2,30 @@
 #include"glut.h"
 
 
-int main(int argc, char** argv)
+void display()
 {
 
-	return 0;
+}
+
+void mouse(int button, int state, int x, int y)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(480, 480);
+    glutInitWindowPosition(0, 95);
+    glutCreateWindow("Checkers");
+    glClearColor(0, 0, 0, 1);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 480, 480, 0, -1.0, 1.0);
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMainLoop();
 }
 
 
